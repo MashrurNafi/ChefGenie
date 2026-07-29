6712bf3c90e72112cbf36a717dc69c56f6d5273f
MashrurNafi <mashrurnafi2k2@gmail.com>
2026-07-29 17:22:59 +0600

    Auth Completed

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,7 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
-
+import { ClerkProvider } from "@clerk/nextjs";
+import { neobrutalism } from "@clerk/themes";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,18 +13,20 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className}`}>
-        <Header/>
-        <main className="min-h-screen">{children}</main>
-        <footer className="py-8 px-4 border-t">
-          <div className="max-w-6xl mx-auto flex justify-center items-center">
-            <p className="text-stone-500 text-sm">
-              Made by Mashrur Jabir & Shahriar
-            </p>
-          </div>
-        </footer>
-      </body>
-    </html>
+    <ClerkProvider appearance={{baseTheme: neobrutalism}}>
+      <html lang="en">
+        <body className={`${inter.className}`}>
+          <Header />
+          <main className="min-h-screen">{children}</main>
+          <footer className="py-8 px-4 border-t">
+            <div className="max-w-6xl mx-auto flex justify-center items-center">
+              <p className="text-stone-500 text-sm">
+                Made by Mashrur Jabir & Shahriar
+              </p>
+            </div>
+          </footer>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- frontend/app/page.js
+++ frontend/app/page.js
@@ -3,8 +3,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      
+    <div className="min-h-screen bg-stone-50 text-stone-900">
+      <section className="pt-32 pb-20 px-4">
+
+      </section>
       <Button variant="primary" size="xl">This App Is Running</Button>
     </div>
   );
